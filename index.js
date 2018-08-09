232e0e406f0e626768d261beccb89bf91d36bef5
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-09 22:42:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-var kittens = ['milo','otis','garfield'];
-
-function destructivelyAppendKitten(name) [kittens.push(name)] {return kittens}
-
+ar kittens = ["Milo", "Otis", "Garfield"]
+ function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+ function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+ function appendKitten(name){
+  var newKittens = [...kittens]
+  newKittens.push(name)
+  return newKittens
+}
+ function prependKitten(name){
+  var newKittens = [...kittens]
+  newKittens.unshift(name)
+  return newKittens
+}
+ function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+ function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+ function removeLastKitten(){
+  var newKittens = [...kittens]
+  newKittens.pop()
+  return newKittens
+}
+ function removeFirstKitten(){
+  var newKittens = [...kittens]
+  newKittens.shift()
+  return newKittens
